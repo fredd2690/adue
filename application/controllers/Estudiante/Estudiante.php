aef9259110de361e363889819da098808189f282
Rodrigo <rodrigo.aspeti@gmail.com>
2020-11-02 03:53:44 -0400

    se agrego toda la interfaz para poder ver la lista de estudiantes (select)

---
+++ application/controllers/Estudiante/Estudiante.php
@@ -0,0 +1,113 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Estudiante extends CI_Controller {
+
+  public function __construct(){
+  parent::__construct();
+  $this->load->model("Estudiante_model");
+  }
+
+	public function index()
+	{
+    $data = array (
+      'estudiantes' => $this->Estudiante_model->getEstudiantes(),
+    );
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/aside');
+		$this->load->view('admin/estudiante/list', $data );
+		$this->load->view('layouts/footer');
+  }
+
+  // public function add()
+  // {
+  //     $this->load->view('layouts/header');
+  //   	$this->load->view('layouts/aside');
+  // 	$this->load->view('admin/curso/add');
+  // 	//$this->load->view('layouts/footer');
+  // }
+  //
+  // public function agregardb()
+  // {
+  //   $curso = $this->input->post("nombre");
+  //   $seccion = $this->input->post("seccion");
+  //   $tutor =$this->input->post("tutor");
+  //   $data = array('nombre' => $curso,
+  //                 'seccion'=>$seccion,
+  //                 'tutor'=>$tutor,
+  //               );
+  //   if ($this->Curso_model->agregarcurso($data)) {
+  //      redirect("Curso/Curso/index");
+  //   }else {
+  //     $this->session->set_flashdata("error","No se pudo guardar la informacion");
+  //     redirect(base_url()."/Curso/Curso/add");
+  //   }
+  // }
+  //
+  //
+  // public function edit($idcurso)
+  //   {
+  //     $data = array('curso' => $this->Curso_model->getCurso($idcurso));
+  //
+  //     //$this->load->view('layouts/header');
+  //     //$this->load->view('layouts/aside');
+  //   	$this->load->view('admin/curso/edit',$data);
+  //   	//$this->load->view('layouts/footer');
+  //   }
+  //
+  // public function view($idCurso)
+  //   {
+  //     $data = array('curso' => $this->Curso_model->getCurso($idCurso));
+  //
+  //     //$this->load->view('layouts/header');
+  //     //$this->load->view('layouts/aside');
+  //   	$this->load->view('admin/curso/view',$data);
+  //   	//$this->load->view('layouts/footer');
+  //   }
+  //
+  // public function updatedb()
+  //   {
+  //     $idcurso = $this->input->post("idcurso");
+  //     $nombre = $this->input->post("nombre");
+  //     $seccion = $this->input->post("seccion");
+  //     $tutor =$this->input->post("tutor");
+  //
+  //
+  //     $data = array('idcurso' => $idcurso,
+  //                   'nombre'=>$nombre,
+  //                   'seccion'=>$seccion,
+  //                   'tutor'=>$tutor,
+  //                 );
+  //     if ($this->Curso_model->update($idcurso,$data)) {
+  //        redirect("Curso/Curso/index");
+  //     }else {
+  //       $this->session->set_flashdata("error","No se pudo Actualizasr la informacion");
+  //       redirect(base_url()."/Curso/curso/edit".$idcurso);
+  //     }
+  //
+  //   }
+  //
+  // public function delete($idcurso)
+  //   {
+  //     $data['estado']='0';
+  //     //$data = array('usuario' => $this->Usuarios_model->deleteUsuario($idusuario,$data));
+  //
+  //     //$this->load->view('layouts/header');
+  //     //$this->load->view('layouts/aside');
+  //   	//$this->load->view('admin/usuarios/',$data);
+  //     //$this->load->view('layouts/footer');
+  //
+  //
+  //     if ($this->Curso_model->deleteCurso($idcurso,$data)) {
+  //       redirect("Curso/Curso/index");
+  //    }else {
+  //      $this->session->set_flashdata("error","No se pudo Actualizasr la informacion");
+  //      redirect(base_url()."/Curso/Curso/");
+  //    }
+  //   }
+
+
+
+
+
+}
---
+++ application/views/admin/estudiante/list.php
@@ -0,0 +1,132 @@
+<!-- START Main section-->
+<section>
+   <!-- START Page content-->
+   <section class="main-content">
+      <a href="<?php echo base_url();?>Curso/Curso/add" class="btn btn-primary btn-labeled pull-right" data-toggle="modal" data-target="#myModal">
+        <span class="btn-label"><i class="fa fa-plus-circle"></i></span>DEFINIR
+      </a>
+      <h3>Estudiantes
+         <br>
+         <small>Lista</small>
+      </h3>
+
+      <!-- START DATATABLE 3-->
+      <div class="row">
+         <div class="col-lg-12">
+            <div class="panel panel-default">
+               <div class="panel-heading">Lista de estudiantes |
+                  <small>detalles</small>
+               </div>
+               <div class="panel-body">
+                  <table id="datatable3" class="table table-striped table-hover">
+                     <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Nombre</th>
+                          <th>Cedula</th>
+                          <th>Sexo</th>
+                          <th>Fecha de Nacimiento</th>
+                          <th>Matricula</th>
+                          <th>Celular</th>
+                          <th>Direccion</th>
+                          <th>Curso</th>
+                          <th class="sort-alpha">acciones</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                       <?php  if(!empty($estudiantes)):?>
+                         <?php $cont = 1; ?>
+                         <?php foreach ($estudiantes as $estudiante): ?>
+                               <tr class="gradeX">
+                                  <td><?php echo $cont; ?></td>
+                                  <td><?php echo $estudiante->nombres." ".$estudiante->aPaterno." ".$estudiante->aMaterno; ?></td>
+                                  <td><?php echo $estudiante->ci; ?></td>
+                                  <td><?php echo $estudiante->sexo; ?></td>
+                                  <td><?php echo $estudiante->fechaNacimiento; ?></td>
+                                  <td><?php echo $estudiante->matricula; ?></td>
+                                  <td><?php echo $estudiante->celular; ?></td>
+                                  <td><?php echo $estudiante->direccion; ?></td>
+                                  <td><?php echo $estudiante->nombre." ".$estudiante->seccion; ?></td>
+                                  <td>
+                                    <div class="btn-group">
+
+                                      <a href="<?php echo base_url();?>curso/curso/view/<?php echo $estudiante->idEstudiante;?>"
+                                         class="btn btn-oval btn-primary" data-toggle="modal" data-target="#myModal"> <span class="fa fa-search"></span> </a>
+                                      <a href="<?php echo base_url();?>curso/curso/edit/<?php echo $estudiante->idEstudiante;?>"
+                                         class="btn btn-warning" data-toggle="modal" data-target="#myModal"> <span class="fa fa-pencil"></span> </a>
+                                      <button data-toggle="modal" data-target="#myModal1" class="btn btn-oval btn-danger "> <span class="fa fa-trash-o"></span> </button>
+
+                                    </div>
+                                  </td>
+                              </tr>
+                              <?php $cont++; ?>
+                        <?php endforeach; ?>
+                      <?php endif; ?>
+                        <tfoot>
+                           <tr>
+                             <th>#</th>
+                             <th>Nombre</th>
+                             <th>Cedula</th>
+                             <th>Sexo</th>
+                             <th>Fecha de Nacimiento</th>
+                             <th>Matricula</th>
+                             <th>Celular</th>
+                             <th>Direccion</th>
+                             <th>Curso</th>
+                             <th class="sort-alpha">acciones</th>
+                           </tr>
+                        </tfoot>
+                     </tbody>
+                  </table>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- END DATATABLE 3-->
+
+   </section>
+   <!-- END Page content-->
+</section>
+<!-- END Main section-->
+
+
+</section>
+<!-- END Main wrapper-->
+
+
+<!-- START modal-->
+<div id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" class="modal fade">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+         </div>
+            <div class="modal-body">
+            </div>
+         <div class="modal-footer">
+         </div>
+      </div>
+   </div>
+</div>
+
+<div id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" class="modal fade">
+   <div class="modal-dialog">
+      <div class="modal-content">
+         <div class="modal-header">
+            <button type="button" data-dismiss="modal" aria-hidden="true" class="close">×</button>
+            <h4 id="myModalLabel" class="modal-title">Eliminar Registro</h4>
+         </div>
+            <div class="modal-body alert alert-danger alert-dismissable ">
+              Esta seguro de quere eliminar este registro?
+            </div>
+         <div class="modal-footer">
+
+            <a href="<?php echo base_url()?>Curso/Curso/delete/<?php echo $estudiante->idEstudiante;?>"
+                 class="btn btn-danger btn-labeled">
+                 <span class="btn-label"> <i class="fa fa-trash-o"></i></span>Eliminar</a>
+            <button type="button" data-dismiss="modal" class="btn btn-primary">Cancelar</button>
+
+         </div>
+      </div>
+   </div>
+</div>
+ <!-- END modal-->
